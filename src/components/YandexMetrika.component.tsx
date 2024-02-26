fdf2f3bfa88cb96c6418d62af50e3e952c5c851d
xTCry [Vladislav Kh] <wolt6@mail.ru>
2024-02-26 22:35:43 +0300

    chore: add remover utms from query

--- src/components/YandexMetrika.component.tsx
+++ src/components/YandexMetrika.component.tsx
@@ -1,7 +1,50 @@
+import * as React from 'react';
 import { YMInitializer } from '@appigram/react-yandex-metrika';
 
 export const YandexMetrika = () => {
     const YM_ID = Number(process.env.REACT_APP_YM_ID);
+
+    const [utmFixed, setUtmFixed] = React.useState(false);
+
+    const removeUTMs = React.useCallback(() => {
+        if (utmFixed || !window.URLSearchParams || !window.history) {
+            return;
+        }
+
+        const params = new URLSearchParams(window.location.search);
+        let urlShouldBeCleaned = false;
+        for (const paramKey of [...params.keys()]) {
+            if (paramKey.startsWith('utm_')) {
+                params.delete(paramKey);
+                urlShouldBeCleaned = true;
+            }
+        }
+
+        if (urlShouldBeCleaned) {
+            const query = params.size > 0 ? '?' + params.toString() : '';
+            const cleanUrl = window.location.pathname + query + window.location.hash;
+            window.history.replaceState(null, '', cleanUrl);
+        }
+        setUtmFixed(true);
+    }, [utmFixed]);
+
+    React.useEffect(() => {
+        let timeout: NodeJS.Timeout | null = null;
+        const handleReinit = () => {
+            removeUTMs();
+        };
+
+        const eventType = `yacounter${YM_ID}inited`;
+
+        window.addEventListener(eventType, handleReinit);
+        timeout = setTimeout(handleReinit, 3e3);
+
+        return () => {
+            window.removeEventListener(eventType, handleReinit);
+            timeout && clearTimeout(timeout);
+        };
+    }, []);
+
     if (!YM_ID) return null;
 
     return (
